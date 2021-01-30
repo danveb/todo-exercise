ad5789f9104090a98f86ebcb95f89d933e74292c
Daniel Bae <danibae91@icloud.com>
2021-01-30 12:19:16 -0800

    review comments and refactor code

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ const ul = document.getElementById('todoList');
 const clearBtn = document.querySelector('.clear'); 
 
 // manipulate form 
+// -- check for form 
 form.addEventListener('submit', function(e) {
-  // check for input value and then do the operation 
+  // -- check for input value and then do the operation 
   if(input.value === '' && input.value.length === 0) {
     alert('Please enter a valid task'); 
   } else if(input.value.length > 0) {
     // create li 
     const li = document.createElement('li'); 
+    // if(todoInput.value.trim().length > 0){}
     // add text to li 
     li.innerText = input.value; 
 
@@ -28,7 +30,8 @@ form.addEventListener('submit', function(e) {
     // append li to ul 
     ul.append(li); 
 
-    // reset form 
+    // reset form to clear all values 
+    // todoInput.value = ''; 
     form.reset(); 
 
     // prevent default behavior (page refresh) 
@@ -36,7 +39,7 @@ form.addEventListener('submit', function(e) {
   };
 }); 
 
-// manipulate ul 
+// manipulate ul for event delegation (handle line-through or remove li)
 ul.addEventListener('click', function(e) {
   // if tagName.toLowerCase() 
   const tagLowerCase = e.target.tagName.toLowerCase(); 
@@ -44,17 +47,22 @@ ul.addEventListener('click', function(e) {
     // const lineThrough = 'line-through'; 
     // e.target.style.textDecoration = lineThrough; 
     // li.target.toggle(lineThrough); 
-    // toggle class 'completed'
+    // toggle class 'completed' (sets line-through in css) 
     e.target.classList.toggle('completed'); 
   } else if(tagLowerCase === 'button') {
+    // remove (parentNode) 
     e.target.parentNode.remove(); 
   }
 })
 
+// manipulate clearBtn to remove all li's
 clearBtn.addEventListener('click', function() {
   // console.log('clicked'); 
+  // select all li's
   const lis = document.querySelectorAll('li'); 
+  // for of loop through li's
   for(li of lis) {
+    // remove() 
     li.remove(); 
   }
 })
